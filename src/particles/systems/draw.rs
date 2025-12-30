f8b1ca6c536681259de65f848d82f80e8678de56
Alessandro Baségio <mestredns@gmail.com>
2025-12-30 01:00:44 -0300

    draw particles passa a buscar particulas e desenhar radius individual

--- src/particles/systems/draw.rs
+++ src/particles/systems/draw.rs
@@ -2,10 +2,8 @@ use bevy::prelude::*;
 
 use crate::particles::components::Particle;
 
-pub fn draw_particles(mut gizmos: Gizmos, query: Query<&Transform, With<Particle>>) {
-    for transform in &query {
-        // TODO: estudar como puxar Particle junto com Transform
-        let particle_radius = 4.0;
-        gizmos.circle_2d(transform.translation.xy(), particle_radius, Color::WHITE);
+pub fn draw_particles(mut gizmos: Gizmos, query: Query<(&Particle, &Transform)>) {
+    for (particle, transform) in &query {
+        gizmos.circle_2d(transform.translation.xy(), particle.radius, Color::WHITE);
     }
 }
