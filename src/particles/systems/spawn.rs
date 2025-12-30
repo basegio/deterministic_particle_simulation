e277bec36df66b2f6ddf1aacbc407af7e883e72a
Alessandro Baségio <mestredns@gmail.com>
2025-12-30 00:35:39 -0300

    particle spawn recebe settings do grid e da simulação

--- src/particles/systems/spawn.rs
+++ src/particles/systems/spawn.rs
@@ -1,17 +1,24 @@
 use bevy::prelude::*;
 
+use crate::grid::resource::CollisionGrid;
 use crate::particles::components::Particle;
 use crate::simulation::resources::SimulationSettings;
 
-pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>) {
-    for i in 1..64 {
-        for j in 1..64 {
+pub fn spawn_particles(
+    mut commands: Commands,
+    settings: Res<SimulationSettings>,
+    grid_settings: Res<CollisionGrid>,
+) {
+    for i in 1..grid_settings.size {
+        for j in 1..grid_settings.size {
             if i % 2 != 0 || j % 2 != 0 {
                 continue;
             }
 
-            let x: f32 = i as f32 * 8.0 - settings.size / 2.0;
-            let y: f32 = j as f32 * 8.0 - settings.size / 2.0;
+            let max_particle_diameter = settings.max_particle_radius * 2.0;
+            let half_simulation_size = settings.size / 2.0;
+            let x: f32 = i as f32 * max_particle_diameter - half_simulation_size;
+            let y: f32 = j as f32 * max_particle_diameter - half_simulation_size;
             let position: Vec2 = Vec2 { x: x, y: y };
             commands.spawn((
                 Particle::new(position, settings.max_particle_radius),
