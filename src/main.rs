01c95c2ebac7e0fbd46b4c064a378643878569ac
Alessandro Baségio <mestredns@gmail.com>
2025-12-31 00:23:45 -0300

    Tamanho da janela fixado em 720x720

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ use simulation::SimulationPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (720, 720).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_plugins(CorePlugin)
         .add_plugins(SimulationPlugin)
