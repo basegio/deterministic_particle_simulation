e95ec3e558fc5da8e471838e72aa6100b71a704a
Alessandro Baségio <mestredns@gmail.com>
2026-01-11 20:17:05 -0300

    spawn refactor

--- src/particles/systems/spawn.rs
+++ src/particles/systems/spawn.rs
@@ -8,16 +8,15 @@ use crate::simulation::resources::SimulationSettings;
 pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>) {
     let mut rng = SmallRng::seed_from_u64(0);
     let simulation_size = settings.size as f32;
+    let max_particle_diameter = settings.max_particle_radius * 2.0;
+    let half_particle_radius = settings.max_particle_radius / 2.0;
+    let half_simulation_size = simulation_size / 2.0;
 
     for i in 1..settings.size {
         for j in 1..settings.size {
             if i % 2 != 0 || j % 2 != 0 {
                 continue;
             }
-
-            let max_particle_diameter = settings.max_particle_radius * 2.0;
-            let half_particle_radius = settings.max_particle_radius / 2.0;
-            let half_simulation_size = simulation_size / 2.0;
 
             let x: f32 = i as f32 * max_particle_diameter - half_simulation_size;
             let y: f32 = j as f32 * max_particle_diameter - half_simulation_size;
