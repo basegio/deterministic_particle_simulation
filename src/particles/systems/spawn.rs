32f1bbbfac3098085bdc520411a151c8900bc16d
Alessandro Baségio <mestredns@gmail.com>
2026-01-11 20:16:16 -0300

    Adicionado raio procedural para particulas

--- src/particles/systems/spawn.rs
+++ src/particles/systems/spawn.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
 
 use crate::particles::components::Particle;
 use crate::simulation::resources::SimulationSettings;
 
 pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>) {
+    let mut rng = SmallRng::seed_from_u64(0);
     let simulation_size = settings.size as f32;
+
     for i in 1..settings.size {
         for j in 1..settings.size {
             if i % 2 != 0 || j % 2 != 0 {
@@ -12,7 +16,9 @@ pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>
             }
 
             let max_particle_diameter = settings.max_particle_radius * 2.0;
+            let half_particle_radius = settings.max_particle_radius / 2.0;
             let half_simulation_size = simulation_size / 2.0;
+
             let x: f32 = i as f32 * max_particle_diameter - half_simulation_size;
             let y: f32 = j as f32 * max_particle_diameter - half_simulation_size;
             let position: Vec2 = Vec2 { x: x, y: y };
@@ -23,8 +29,9 @@ pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>
                 continue;
             };
 
+            let radius = half_particle_radius + (half_particle_radius * rng.random::<f32>());
             commands.spawn((
-                Particle::new(position, settings.max_particle_radius),
+                Particle::new(position, radius),
                 Transform::from_xyz(position.x, position.y, 0.0),
             ));
         }
