f25489ef62d11f15285ef137c486f6f78a55baa1
Alessandro Baségio <mestredns@gmail.com>
2026-01-12 23:13:22 -0300

    Alterado padrão de spawn

--- src/particles/systems/spawn.rs
+++ src/particles/systems/spawn.rs
@@ -13,15 +13,11 @@ pub fn spawn_particles(mut commands: Commands, settings: Res<SimulationSettings>
 
     for i in 1..settings.size {
         for j in 1..settings.size {
-            if i % 2 != 0 || j % 2 != 0 {
-                continue;
-            }
-
             let x: f32 = i as f32 * max_particle_diameter - half_simulation_size;
             let y: f32 = j as f32 * max_particle_diameter - half_simulation_size;
             let position: Vec2 = Vec2 { x: x, y: y };
 
-            if position.distance(Vec2::ZERO) < simulation_size * 0.04
+            if position.distance(Vec2::ZERO) < simulation_size * 0.08
                 || position.distance(Vec2::ZERO) > simulation_size * 0.48
             {
                 continue;
